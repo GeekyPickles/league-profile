ee53416529dff2a282bc5c6683b5550734f798c0
GeekyPickles <geekypickles@gmail.com>
2019-07-08 18:13:40 -0700

    Moved Riot API calls to PHP

--- main.js
+++ main.js
@@ -16,13 +16,14 @@
   const OPGG_URL = "http://opgg-static.akamaized.net/images/profile_icons/profileIcon";
   const CHAMPION_MASTERY = "/lol/champion-mastery/v4/champion-masteries/by-summoner/";
   // API Key, needs to be updated every 24 hrs
-  const API_KEY = "?api_key=RGAPI-ec0f8340-8561-4111-a87a-1ed9c66248f8";
+  const API_KEY = "?api_key=RGAPI-f74bd49f-d4bd-448d-a5df-3ebcfc0d1bff";
 
   const DDRAGON_URL = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/";
   const DDRAGON_SPLASH = "champion/splash/";
 
   // new requests
   const YORHA_SUMMONERS_BY_NAME = "?summoners=";
+  const YORHA_MASTERY = "?mastery=";
   const YORHA_MATCHLIST = "?matchlist=";
   const YORHA_MATCH = "?matches=";
   // pod URL suffix
@@ -78,13 +79,15 @@
       clearSummonerPage();
 
 
-      makeRequest(PROXY_URL + RIOT_URL + SUMMONERS_BY_NAME + summonerName + API_KEY, createSummonerPage, checkStatusJSON);
+      makeRequest(URL + YORHA_SUMMONERS_BY_NAME + summonerName, createSummonerPage, checkStatusJSON);
     });
 
     makeRequest(URL + ALL_OPS, loadCurrentOps, checkStatusPlainText);
     scrambleTargets = qsa("h1, h2, h3, p, a, li");
     images = qsa("img");
   }
+
+
    /**
    *  This function loads the names of the current YoRha operators
    *  @param {Plain Text} responseData - plain text returned from the GET request with all three
@@ -143,7 +146,7 @@
     generalInfo.appendChild(summonerLevelDisplay);
     generalInfo.setAttribute("id", "general-info");
 
-    makeRequest(PROXY_URL + RIOT_URL + CHAMPION_MASTERY + responseData["id"] + API_KEY,
+    makeRequest(URL + YORHA_MASTERY + responseData["id"],
                 loadMastery, checkStatusJSON);
   }
 
@@ -187,7 +190,7 @@
 
     }
     else {
-
+      // error handling
     }
     generalInfo.appendChild(masteryContainer);
     let matchHistoryButton = document.createElement("button");
@@ -195,10 +198,21 @@
     matchHistoryButton.setAttribute("id", "match-history-button");
     generalInfo.appendChild(matchHistoryButton);
     pageOwnerId = summonerData["accountId"];
+
+    // preload first 20 matches and set to hidden?
     matchHistoryButton.addEventListener("click", function() {
-      makeRequest(PROXY_URL + RIOT_URL + MATCHLIST + pageOwnerId + API_KEY,
+      makeRequest(URL + YORHA_MATCHLIST + pageOwnerId,
                   loadMatchHistory, checkStatusJSON);
     });
+    loadWinrate();
+  }
+
+  function loadWinrate() {
+    let infoBox = document.getElementById("general-info");
+    let pieChart = document.createElement("svg");
+    let svgContainer = document.createElement("g");
+
+
   }
 
   function loadMatchHistory(responseData) {
@@ -206,7 +220,7 @@
     let matchHistoryBox = document.createElement("div");
     let infoDisplay = document.getElementById("info-display");
 
-    //making new button
+    //making load matches button
     let addMatches = document.createElement("button");
     addMatches.addEventListener("click", function() {
       loadMatches();
@@ -304,7 +318,7 @@
 
     matchHistoryBox.appendChild(newMatch);
     newMatch.addEventListener("click", openMatch);
-    makeRequest(PROXY_URL + RIOT_URL + MATCH + match["gameId"] + API_KEY,
+    makeRequest(URL + YORHA_MATCH + match["gameId"],
                 fillTeams, checkStatusJSON);
     // makeRequest(PROXY_URL + RIOT_URL + MATCH + matchId + API_KEY, createMatchHistory, checkStatusJSON);
   }
@@ -345,7 +359,7 @@
       playerDiv.addEventListener("click", function(){
         let summonerName = playerName.innerText;
         clearSummonerPage();
-        makeRequest(PROXY_URL + RIOT_URL + SUMMONERS_BY_NAME + summonerName + API_KEY, createSummonerPage, checkStatusJSON);
+        makeRequest(URL + YORHA_SUMMONERS_BY_NAME + summonerName, createSummonerPage, checkStatusJSON);
       });
 
       //changedhere
@@ -376,7 +390,7 @@
           }
           else {
             thisMatch[0].classList.add("loss");
-          }  
+          }
         }
         let kdaBox = document.createElement("div");
         kdaBox.classList.add("kda-box");
@@ -555,6 +569,9 @@
       if (response.status == "429") {
         errorMessage.innerText = "ERROR 429: TOO MANY REQUESTS";
       }
+      else if (response.status == "403") {
+        errorMessage.innerText = "ERROR 404: EXPIRED API KEY";
+      }
       else if (response.status == "404") {
         errorMessage.innerText = "ERROR 404: SUMMONER NOT FOUND";
       }
@@ -657,151 +674,157 @@
   // borrowed from https://github.com/Najsr/League-Of-Legends-Champions-ID-List/blob/master/list.php
   function championIdToName(id) {
     switch(id){
-    case 266: return "Aatrox"; break;
-    case 412: return "Thresh"; break;
-    case 23: return "Tryndamere"; break;
-    case 79: return "Gragas"; break;
-    case 69: return "Cassiopeia"; break;
-    case 136: return "Aurelion_Sol"; break;
-    case 13: return "Ryze"; break;
-    case 78: return "Poppy"; break;
-    case 14: return "Sion"; break;
-    case 1: return "Annie"; break;
-    case 202: return "Jhin"; break;
-    case 43: return "Karma"; break;
-    case 111: return "Nautilus"; break;
-    case 240: return "Kled"; break;
-    case 99: return "Lux"; break;
-    case 103: return "Ahri"; break;
-    case 2: return "Olaf"; break;
-    case 112: return "Viktor"; break;
-    case 34: return "Anivia"; break;
-    case 27: return "Singed"; break;
-    case 86: return "Garen"; break;
-    case 127: return "Lissandra"; break;
-    case 57: return "Maokai"; break;
-    case 25: return "Morgana"; break;
-    case 28: return "Evelynn"; break;
-    case 105: return "Fizz"; break;
-    case 74: return "Heimerdinger"; break;
-    case 238: return "Zed"; break;
-    case 68: return "Rumble"; break;
-    case 82: return "Mordekaiser"; break;
-    case 37: return "Sona"; break;
-    case 96: return "Kog'Maw"; break;
-    case 55: return "Katarina"; break;
-    case 117: return "Lulu"; break;
-    case 22: return "Ashe"; break;
-    case 30: return "Karthus"; break;
-    case 12: return "Alistar"; break;
-    case 122: return "Darius"; break;
-    case 67: return "Vayne"; break;
-    case 110: return "Varus"; break;
-    case 77: return "Udyr"; break;
-    case 89: return "Leona"; break;
-    case 126: return "Jayce"; break;
-    case 134: return "Syndra"; break;
-    case 80: return "Pantheon"; break;
-    case 92: return "Riven"; break;
-    case 121: return "Kha'Zix"; break;
-    case 42: return "Corki"; break;
-    case 268: return "Azir"; break;
-    case 51: return "Caitlyn"; break;
-    case 76: return "Nidalee"; break;
-    case 85: return "Kennen"; break;
-    case 3: return "Galio"; break;
-    case 45: return "Veigar"; break;
-    case 432: return "Bard"; break;
-    case 150: return "Gnar"; break;
-    case 90: return "Malzahar"; break;
-    case 104: return "Graves"; break;
-    case 254: return "Vi"; break;
-    case 10: return "Kayle"; break;
-    case 39: return "Irelia"; break;
-    case 64: return "Lee_Sin"; break;
-    case 420: return "Illaoi"; break;
-    case 60: return "Elise"; break;
-    case 106: return "Volibear"; break;
-    case 20: return "Nunu"; break;
-    case 4: return "Twisted_Fate"; break;
-    case 24: return "Jax"; break;
-    case 102: return "Shyvana"; break;
-    case 429: return "Kalista"; break;
-    case 36: return "Dr._Mundo"; break;
-    case 427: return "Ivern"; break;
-    case 131: return "Diana"; break;
-    case 223: return "Tahm_Kench"; break;
-    case 63: return "Brand"; break;
-    case 113: return "Sejuani"; break;
-    case 8: return "Vladimir"; break;
-    case 154: return "Zac"; break;
-    case 421: return "Rek'Sai"; break;
-    case 133: return "Quinn"; break;
-    case 84: return "Akali"; break;
-    case 163: return "Taliyah"; break;
-    case 18: return "Tristana"; break;
-    case 120: return "Hecarim"; break;
-    case 15: return "Sivir"; break;
-    case 236: return "Lucian"; break;
-    case 107: return "Rengar"; break;
-    case 19: return "Warwick"; break;
-    case 72: return "Skarner"; break;
-    case 54: return "Malphite"; break;
-    case 157: return "Yasuo"; break;
-    case 101: return "Xerath"; break;
-    case 17: return "Teemo"; break;
-    case 75: return "Nasus"; break;
-    case 58: return "Renekton"; break;
-    case 119: return "Draven"; break;
-    case 35: return "Shaco"; break;
-    case 50: return "Swain"; break;
-    case 91: return "Talon"; break;
-    case 40: return "Janna"; break;
-    case 115: return "Ziggs"; break;
-    case 245: return "Ekko"; break;
-    case 61: return "Orianna"; break;
-    case 114: return "Fiora"; break;
-    case 9: return "Fiddlesticks"; break;
-    case 31: return "Cho'Gath"; break;
-    case 33: return "Rammus"; break;
-    case 7: return "LeBlanc"; break;
-    case 16: return "Soraka"; break;
-    case 26: return "Zilean"; break;
-    case 56: return "Nocturne"; break;
-    case 222: return "Jinx"; break;
-    case 83: return "Yorick"; break;
-    case 6: return "Urgot"; break;
-    case 203: return "Kindred"; break;
-    case 21: return "Miss_Fortune"; break;
-    case 62: return "Wukong"; break;
-    case 53: return "Blitzcrank"; break;
-    case 98: return "Shen"; break;
-    case 201: return "Braum"; break;
-    case 5: return "Xin_Zhao"; break;
-    case 29: return "Twitch"; break;
-    case 11: return "Master_Yi"; break;
-    case 44: return "Taric"; break;
-    case 32: return "Amumu"; break;
-    case 41: return "Gangplank"; break;
-    case 48: return "Trundle"; break;
-    case 38: return "Kassadin"; break;
-    case 161: return "Vel'Koz"; break;
-    case 143: return "Zyra"; break;
-    case 267: return "Nami"; break;
-    case 59: return "Jarvan_IV"; break;
-    case 81: return "Ezreal"; break;
-    case 145: return "Kai'Sa"; break;
-    case 555: return "Pyke"; break;
-    case 518: return "Neeko"; break;
-    case 141: return "Kayn"; break;
-    case 498: return "Xayah"; break;
-    case 350: return "Yuumi"; break;
-    case 516: return "Ornn"; break;
-    case 517: return "Sylas"; break;
-    case 142: return "Zoe"; break;
-    case 164: return "Camille"; break;
-    case 497: return "Rakan"; break;
+      case 266: return "Aatrox"; break;
+      case 412: return "Thresh"; break;
+      case 23: return "Tryndamere"; break;
+      case 79: return "Gragas"; break;
+      case 69: return "Cassiopeia"; break;
+      case 136: return "Aurelion_Sol"; break;
+      case 13: return "Ryze"; break;
+      case 78: return "Poppy"; break;
+      case 14: return "Sion"; break;
+      case 1: return "Annie"; break;
+      case 202: return "Jhin"; break;
+      case 43: return "Karma"; break;
+      case 111: return "Nautilus"; break;
+      case 240: return "Kled"; break;
+      case 99: return "Lux"; break;
+      case 103: return "Ahri"; break;
+      case 2: return "Olaf"; break;
+      case 112: return "Viktor"; break;
+      case 34: return "Anivia"; break;
+      case 27: return "Singed"; break;
+      case 86: return "Garen"; break;
+      case 127: return "Lissandra"; break;
+      case 57: return "Maokai"; break;
+      case 25: return "Morgana"; break;
+      case 28: return "Evelynn"; break;
+      case 105: return "Fizz"; break;
+      case 74: return "Heimerdinger"; break;
+      case 238: return "Zed"; break;
+      case 68: return "Rumble"; break;
+      case 82: return "Mordekaiser"; break;
+      case 37: return "Sona"; break;
+      case 96: return "Kog'Maw"; break;
+      case 55: return "Katarina"; break;
+      case 117: return "Lulu"; break;
+      case 22: return "Ashe"; break;
+      case 30: return "Karthus"; break;
+      case 12: return "Alistar"; break;
+      case 122: return "Darius"; break;
+      case 67: return "Vayne"; break;
+      case 110: return "Varus"; break;
+      case 77: return "Udyr"; break;
+      case 89: return "Leona"; break;
+      case 126: return "Jayce"; break;
+      case 134: return "Syndra"; break;
+      case 80: return "Pantheon"; break;
+      case 92: return "Riven"; break;
+      case 121: return "Kha'Zix"; break;
+      case 42: return "Corki"; break;
+      case 268: return "Azir"; break;
+      case 51: return "Caitlyn"; break;
+      case 76: return "Nidalee"; break;
+      case 85: return "Kennen"; break;
+      case 3: return "Galio"; break;
+      case 45: return "Veigar"; break;
+      case 432: return "Bard"; break;
+      case 150: return "Gnar"; break;
+      case 90: return "Malzahar"; break;
+      case 104: return "Graves"; break;
+      case 254: return "Vi"; break;
+      case 10: return "Kayle"; break;
+      case 39: return "Irelia"; break;
+      case 64: return "Lee_Sin"; break;
+      case 420: return "Illaoi"; break;
+      case 60: return "Elise"; break;
+      case 106: return "Volibear"; break;
+      case 20: return "Nunu"; break;
+      case 4: return "Twisted_Fate"; break;
+      case 24: return "Jax"; break;
+      case 102: return "Shyvana"; break;
+      case 429: return "Kalista"; break;
+      case 36: return "Dr._Mundo"; break;
+      case 427: return "Ivern"; break;
+      case 131: return "Diana"; break;
+      case 223: return "Tahm_Kench"; break;
+      case 63: return "Brand"; break;
+      case 113: return "Sejuani"; break;
+      case 8: return "Vladimir"; break;
+      case 154: return "Zac"; break;
+      case 421: return "Rek'Sai"; break;
+      case 133: return "Quinn"; break;
+      case 84: return "Akali"; break;
+      case 163: return "Taliyah"; break;
+      case 18: return "Tristana"; break;
+      case 120: return "Hecarim"; break;
+      case 15: return "Sivir"; break;
+      case 236: return "Lucian"; break;
+      case 107: return "Rengar"; break;
+      case 19: return "Warwick"; break;
+      case 72: return "Skarner"; break;
+      case 54: return "Malphite"; break;
+      case 157: return "Yasuo"; break;
+      case 101: return "Xerath"; break;
+      case 17: return "Teemo"; break;
+      case 75: return "Nasus"; break;
+      case 58: return "Renekton"; break;
+      case 119: return "Draven"; break;
+      case 35: return "Shaco"; break;
+      case 50: return "Swain"; break;
+      case 91: return "Talon"; break;
+      case 40: return "Janna"; break;
+      case 115: return "Ziggs"; break;
+      case 245: return "Ekko"; break;
+      case 61: return "Orianna"; break;
+      case 114: return "Fiora"; break;
+      case 9: return "Fiddlesticks"; break;
+      case 31: return "Cho'Gath"; break;
+      case 33: return "Rammus"; break;
+      case 7: return "LeBlanc"; break;
+      case 16: return "Soraka"; break;
+      case 26: return "Zilean"; break;
+      case 56: return "Nocturne"; break;
+      case 222: return "Jinx"; break;
+      case 83: return "Yorick"; break;
+      case 6: return "Urgot"; break;
+      case 203: return "Kindred"; break;
+      case 21: return "Miss_Fortune"; break;
+      case 62: return "Wukong"; break;
+      case 53: return "Blitzcrank"; break;
+      case 98: return "Shen"; break;
+      case 201: return "Braum"; break;
+      case 5: return "Xin_Zhao"; break;
+      case 29: return "Twitch"; break;
+      case 11: return "Master_Yi"; break;
+      case 44: return "Taric"; break;
+      case 32: return "Amumu"; break;
+      case 41: return "Gangplank"; break;
+      case 48: return "Trundle"; break;
+      case 38: return "Kassadin"; break;
+      case 161: return "Vel'Koz"; break;
+      case 143: return "Zyra"; break;
+      case 267: return "Nami"; break;
+      case 59: return "Jarvan_IV"; break;
+      case 81: return "Ezreal"; break;
+      case 145: return "Kai'Sa"; break;
+      case 555: return "Pyke"; break;
+      case 518: return "Neeko"; break;
+      case 141: return "Kayn"; break;
+      case 498: return "Xayah"; break;
+      case 350: return "Yuumi"; break;
+      case 516: return "Ornn"; break;
+      case 517: return "Sylas"; break;
+      case 142: return "Zoe"; break;
+      case 164: return "Camille"; break;
+      case 497: return "Rakan"; break;
     }
     return id;
+  }
+
+  function itemIdToName(id) {
+    switch(id) {
+
+    }
   }
 })();
--- yorha.php
+++ yorha.php
@@ -1,12 +1,13 @@
 <?php
   $RIOT_URL = "https://na1.api.riotgames.com";
   $SUMMONERS_BY_NAME = "/lol/summoner/v4/summoners/by-name/";
+  $CHAMPION_MASTERY = "/lol/champion-mastery/v4/champion-masteries/by-summoner/";
   $MATCHLIST = "/lol/match/v4/matchlists/by-account/";
   $MATCH = "/lol/match/v4/matches/";
   $PROXY_URL = "https://cors-anywhere.herokuapp.com/";
   $OPGG_URL = "http://opgg-static.akamaized.net/images/profile_icons/profileIcon";
   // API Key, needs to be updated every 24 hrs
-  $API_KEY = "?api_key=RGAPI-299f4e9a-ef18-4838-b017-59829014e729";
+  $API_KEY = "?api_key=RGAPI-f74bd49f-d4bd-448d-a5df-3ebcfc0d1bff";
 
   $DDRAGON_URL = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/";
   $DDRAGON_SPLASH = "champion/splash/";
@@ -70,9 +71,32 @@
   }
 
   else if (isset($_GET["summoners"])) {
+    header("Content-Type: application/json");
     $summoners = $_GET["summoners"];
-    echo "test";
-    makeRequest(PROXY_URL + RIOT_URL + SUMMONERS_BY_NAME + summonerName + API_KEY, responseData, checkStatusJSON);
+    $response = file_get_contents($RIOT_URL . $SUMMONERS_BY_NAME . $summoners . $API_KEY);
+    echo $response;
+    // makeRequest(PROXY_URL + RIOT_URL + SUMMONERS_BY_NAME + summonerName + API_KEY, responseData, checkStatusJSON);
+  }
+
+  else if (isset($_GET["mastery"])) {
+    header("Content-Type: application/json");
+    $id = $_GET["mastery"];
+    $response = file_get_contents($RIOT_URL . $CHAMPION_MASTERY . $id . $API_KEY);
+    echo $response;
+  }
+
+  else if (isset($_GET["matchlist"])) {
+    header("Content-Type: application/json");
+    $id = $_GET["matchlist"];
+    $response = file_get_contents($RIOT_URL . $MATCHLIST . $id . $API_KEY);
+    echo $response;
+  }
+
+  else if (isset($_GET["matches"])) {
+    header("Content-Type: application/json");
+    $matchid = $_GET["matches"];
+    $response = file_get_contents($RIOT_URL . $MATCH . $matchid . $API_KEY);
+    echo $response;
   }
 
   else {
@@ -80,11 +104,11 @@
     echo "Missing required 'pods' or 'operators' GET parameter";
   }
 
-
-  function printJSON($responseData) {
-    header("Content-Type: application/json");
-    echo json_encode(responseData);
-  }
+  //
+  // function printJSON($responseData) {
+  //   header("Content-Type: application/json");
+  //   echo json_encode(responseData);
+  // }
 
   /**
   *  Makes GET request to specified URL and runs specified function if
@@ -92,48 +116,48 @@
   *  @param {String} apiURL - URL that get request is made to
   *  @param {function} successFunction - function to be run if GET request is successful
   */
- function makeRequest($apiURL, $successFunction, $checkStatus) {
-   // CORB blocked, need HTML encoding
-   $myHeaders = new Headers();
-   $myHeaders.append("X-Content-Type-Options","nosniff");
-   $myHeaders.append("Content-Type", "application/json");
-   $myHeaders.append("Access-Control-Allow-Origin", "*");
-
-   fetch($apiURL)
-    .then($checkStatus)
-    .then($successFunction)
-    .catch(echo);
- }
-  /**
- * Helper function to return the response's result text if successful, otherwise
- * returns the rejected Promise result with an error status and corresponding text
- * @param {object} response - response to check for success/error
- * @returns {object} - valid result JSON if response was successful, otherwise rejected
- *                     Promise result
- */
-  function checkStatusJSON($response) {
-    if ($response.status < 300) {
-     echo "source extracted";
-     return $response.json();
-   } else {
-     echo "source: ".$response.json();
-     return Promise.reject(new Error($response.status + ": " + $response.statusText));
-   }
-  }
-  /**
- * Helper function to return the response's result text if successful, otherwise
- * returns the rejected Promise result with an error status and corresponding text
- * @param {object} response - response to check for success/error
- * @returns {object} - valid result text if response was successful, otherwise rejected
- *                     Promise result
- */
-  function checkStatusPlainText($response) {
-    if ($response.status < 300) {
-     echo "source extracted";
-     return $response.text();
-   } else {
-     echo "source: ".$response.json();
-     return Promise.reject(new Error($response.status.": ".$response.statusText));
-   }
-  }
+ // function makeRequest($apiURL, $successFunction, $checkStatus) {
+ //   // CORB blocked, need HTML encoding
+ //   $myHeaders = new Headers();
+ //   $myHeaders.append("X-Content-Type-Options","nosniff");
+ //   $myHeaders.append("Content-Type", "application/json");
+ //   $myHeaders.append("Access-Control-Allow-Origin", "*");
+ //
+ //   fetch($apiURL)
+ //    .then($checkStatus)
+ //    .then($successFunction)
+ //    .catch(echo);
+ // }
+ //  /**
+ // * Helper function to return the response's result text if successful, otherwise
+ // * returns the rejected Promise result with an error status and corresponding text
+ // * @param {object} response - response to check for success/error
+ // * @returns {object} - valid result JSON if response was successful, otherwise rejected
+ // *                     Promise result
+ // */
+ //  function checkStatusJSON($response) {
+ //    if ($response.status < 300) {
+ //     echo "source extracted";
+ //     return $response.json();
+ //   } else {
+ //     echo "source: ".$response.json();
+ //     return Promise.reject(new Error($response.status + ": " + $response.statusText));
+ //   }
+ //  }
+ //  /**
+ // * Helper function to return the response's result text if successful, otherwise
+ // * returns the rejected Promise result with an error status and corresponding text
+ // * @param {object} response - response to check for success/error
+ // * @returns {object} - valid result text if response was successful, otherwise rejected
+ // *                     Promise result
+ // */
+ //  function checkStatusPlainText($response) {
+ //    if ($response.status < 300) {
+ //     echo "source extracted";
+ //     return $response.text();
+ //   } else {
+ //     echo "source: ".$response.json();
+ //     return Promise.reject(new Error($response.status.": ".$response.statusText));
+ //   }
+ //  }
 ?>
